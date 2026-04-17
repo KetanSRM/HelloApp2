6fd79f1c886ebdb008db605a647e4d82ae1735df
KetanSRM <ks9956@srmist.edu.in>
2026-04-18 00:47:50 +0530

    Update Main class to handle command line arguments
    Refactor main method to accept command line arguments and greet users accordingly.

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        System.out.println("Hello, World!");
+public class Main{
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+            String names = "";
+            if (nameBuilder.length() > 0) {
+                names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
