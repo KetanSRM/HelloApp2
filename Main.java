fb9b7b8030aed672abb720d5e8640ab50453500a
KetanSRM <ks9956@srmist.edu.in>
2026-04-18 00:46:57 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        System.out.println("Hello, World!");
+public class Main{
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+            System.out.println("Hello, " + names.toString() + "!");
+        }
     }
 }
